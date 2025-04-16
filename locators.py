84294c9571020e1ad1592f874f712e24360f9988
Dmitriy Malichenko <dmitrymalichenko@yandex.ru>
2025-04-16 19:21:38 +0300

    Commit_2

--- locators.py
+++ locators.py
@@ -2,53 +2,55 @@ from selenium.webdriver.common.by import By
 
 class Locators:
 
-    personal_account_button_1 = By.XPATH, "//*[@id='root']/div/header/nav/a/p"  # Раздел "Личный кабинет". Главная страница
-
-    personal_account_button_2 = By.XPATH, "//p[contains(text(), 'Личный Кабинет'"  # Раздел "Личный кабинет". Главная страница
+    personal_account_button = By.XPATH, "//p[text()='Личный Кабинет']"  # Раздел "Личный кабинет". Главная страница
 
     account_login_button = By.XPATH, "//button[contains(text(), 'Войти в аккаунт')]"  # Кнопка "Войти в аккаунт". Главная страница
 
-    field_email = By.XPATH, "//*[@id='root']/div/main/div/form/fieldset[1]/div/div/input"  # Поле "Email". Страница "Вход"
+    field_email = By.XPATH, "//input[contains(@class,'textfield') and @name='name']"  # Поле "Email". Страница "Вход"
 
-    register = By.XPATH, "//*[@id='root']/div/main/div/div/p[1]/a"  # "Зарегистрироваться". Страница "Вход"
+    register = By.XPATH, "//a[@href='/register']"  # "Зарегистрироваться". Страница "Вход"
 
-    password_field = By.XPATH, "//*[@id='root']/div/main/div/form/fieldset[2]/div/div/input"  # Поле "Пароль". Страница "Вход"
+    password_field = By.XPATH, "//input[contains(@class,'input__textfield') and @type='password']"  # Поле "Пароль". Страница "Вход"
 
-    login_button = By.XPATH, "//button[contains(text(), 'Войти')]"  # Кнопка "Войти" Страница "Вход"
+    login_button = By.XPATH, "//button[text() = 'Войти']"  # Кнопка "Войти" Страница "Вход"
 
-    password_recovery = By.XPATH, "//*[@id='root']/div/main/div/div/p[2]/a"  # "Восстановить пароль". Страница "Вход"
+    password_recovery = By.XPATH, "//a[text()='Восстановить пароль']"  # "Восстановить пароль". Страница "Вход"
 
-    name_registration = By.XPATH, "//*[@id='root']//input[@name='name']"  # Поле "Имя". Страница "Регистрация"
+    name_registration = By.XPATH, "//input[@type='text' and @name='name']"  # Поле "Имя". Страница "Регистрация"
 
-    email_registration = By.XPATH, "//*[@id='root']/div/main/div/form/fieldset[2]/div/div/input"  # Поле "Email". Страница "Регистрация"
+    email_registration = By.XPATH, "(//input[@name='name'])[2]"  # Поле "Email". Страница "Регистрация"
 
-    password_registration = By.XPATH, "//*[@id='root']//input[@name='Пароль']"  # Поле "Пароль". Страница "Регистрация"
+    password_registration = By.XPATH, "//input[@type='password']"  # Поле "Пароль". Страница "Регистрация"
 
-    registration_button = By.XPATH, "//*[@id='root']/div/main/div/form/button"  # Кнопка "Зарегистрироваться". Страница "Регистрация"
+    registration_button = By.XPATH, "//button[text()='Зарегистрироваться']"  # Кнопка "Зарегистрироваться". Страница "Регистрация"
 
-    incorrect_password = By.XPATH, "//*[@id='root']/div/main/div/form/fieldset[3]/div/p"  # Сообщение об ошибки "Некорректный пароль". Страница "Регистрация"
+    incorrect_password = By.XPATH, "//p[text()='Некорректный пароль']"  # Сообщение об ошибки "Некорректный пароль". Страница "Регистрация"
 
-    email_password_recovery = By.XPATH, "//*[@id='root']/div/main/div/form/fieldset/div/div/input"  # Поле "Email". Страница "Восстановление пароля"
+    email_password_recovery = By.XPATH, "//input[@type='text' and @name='name']"  # Поле "Email". Страница "Восстановление пароля"
 
-    recover_button = By.XPATH, "//*[@id='root']/div/main/div/form/button"  # Кнопка "Восстановить". Страница "Восстановление пароля"
+    recover_button = By.XPATH, "//button[text()='Восстановить']"  # Кнопка "Восстановить". Страница "Восстановление пароля"
 
-    constructor = By.XPATH, "//*[@id='root']/div/header/nav/ul/li[1]/a/p"  # Раздел "Конструктор" в шапке сайта.
+    constructor = By.XPATH, "//p[text()='Конструктор']"  # Раздел "Конструктор" в шапке сайта.
 
-    stellar_burgers_logo = By.CSS_SELECTOR, "#root header nav div a svg"  # Логотип Stellar Burgers в шапке сайта
+    stellar_burgers_logo = By.XPATH, "//*[local-name()='svg']//*[local-name()='path'][1]"  # Логотип Stellar Burgers в шапке сайта
 
-    exit_button = By.XPATH, "//*[@id='root']//main//nav//ul//li[3]//button"  # Кнопка "Выход". Страница "Профиль"
+    exit_button = By.XPATH, "//button[text()='Выход']"  # Кнопка "Выход". Страница "Профиль"
 
-    field_name = By.CSS_SELECTOR, "#root>div>main>div>div>div>ul>li:nth-child(1)>div>div>input"  # Окно "Имя". Страница "Профиль"
+    field_name = By.XPATH, "//input[@name='Name']"  # Поле "Имя". Страница "Профиль"
 
-    bun_section = By.XPATH, "//*[@id='root']/div/main/section[1]/div[1]/div[1]/span"  # Раздел "Булки"
+    field_login = By.XPATH, "(//input[contains(@class, 'input__textfield')])[2]" # Поле Логин. Страница "Профиль"
 
-    fluorescent_bun = By.XPATH, "//*[@id='root']/div/main/section[1]/div[2]/ul[1]/a[1]/img"  # Флюоресцентная булка
+    field_password_profile = By.XPATH, "(//input[contains(@class, 'input__textfield')])[3]" # Поле Пароль. Страница "Профиль"
 
-    section_sauces = By.XPATH, "//*[@id='root']/div/main/section[1]/div[1]/div[2]"  # Раздел "Соусы"
+    bun_section = By.XPATH, "//span[.='Булки']" # Раздел "Булки"
 
-    spicy_x = By.XPATH, "//*[@id='root']/div/main/section[1]/div[2]/ul[2]/a[1]"  # Соус Spicy-X
+    fluorescent_bun = By.XPATH, "//img[@alt='Флюоресцентная булка R2-D3']"  # Флюоресцентная булка
 
-    filling_section = By.XPATH, "//*[@id='root']/div/main/section[1]/div[1]/div[3]/span"  # Раздел "Начинки"
+    section_sauces = By.XPATH, "//span[.='Соусы']"  # Раздел "Соусы"
 
-    beef_meteorite = By.XPATH, "//*[@id='root']/div/main/section[1]/div[2]/ul[3]/a[2]/img"  # "Говяжий метеорит(отбивная)"
+    spicy_x = By.XPATH, "//a[contains(., 'Соус Spicy-X')]"  # Соус Spicy-X
+
+    filling_section = By.XPATH, "//span[text()='Начинки']"  # Раздел "Начинки"
+
+    beef_meteorite = By.XPATH, "//p[text()='Говяжий метеорит (отбивная)']"  # "Говяжий метеорит(отбивная)"
 
