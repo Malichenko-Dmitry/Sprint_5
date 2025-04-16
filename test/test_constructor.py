79baedac394bc3279eac116f905093cafbe98f85
Dmitriy Malichenko <dmitrymalichenko@yandex.ru>
2025-04-16 22:32:33 +0300

    Commit_4

--- test/test_constructor.py
+++ test/test_constructor.py
@@ -1,49 +1,40 @@
-import time
-from selenium import webdriver
 from locators import Locators
 import helpers
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class Testconstructor:
+
+    def test_bun_section(self, chrome_driver, open_chrome_site):
+
+        open_chrome_site()
+        helpers.click_element(chrome_driver, Locators.constructor)
+        helpers.click_element(chrome_driver, Locators.bun_section)
+        WebDriverWait(chrome_driver, 10).until(
+            EC.presence_of_element_located(Locators.fluorescent_bun)
+        )
+        assert 'Флюоресцентная булка R2-D3' in chrome_driver.page_source
 
 
-def test_bun_section():
-    driver = webdriver.Chrome()
+    def test_sauce_section(self, chrome_driver, open_chrome_site):
 
-    try:
-        driver.get("https://stellarburgers.nomoreparties.site/")
-        helpers.click_element(driver, Locators.constructor)
-        time.sleep(4)
-        helpers.click_element(driver, Locators.bun_section)
-        time.sleep(4)
-        assert "Булки" in driver.page_source
-
-    finally:
-        driver.quit()
+        open_chrome_site()
+        helpers.click_element(chrome_driver, Locators.constructor)
+        helpers.click_element(chrome_driver, Locators.section_sauces)
+        WebDriverWait(chrome_driver, 10).until(
+            EC.presence_of_element_located(Locators.spicy_x)
+        )
+        assert 'Соус Spicy-X' in chrome_driver.page_source
 
 
-def test_sauce_section():
-    driver = webdriver.Chrome()
 
-    try:
-        driver.get("https://stellarburgers.nomoreparties.site/")
-        helpers.click_element(driver, Locators.constructor)
-        time.sleep(4)
-        helpers.click_element(driver, Locators.section_sauces)
-        time.sleep(4)
-        assert "Соусы" in driver.page_source
+    def test_fillings_section(self, chrome_driver, open_chrome_site):
 
-    finally:
-        driver.quit()
+        open_chrome_site()
+        helpers.click_element(chrome_driver, Locators.constructor)
+        helpers.click_element(chrome_driver, Locators.filling_section)
+        WebDriverWait(chrome_driver, 10).until(
+            EC.presence_of_element_located(Locators.beef_meteorite)
+        )
+        assert 'Говяжий метеорит (отбивная)' in chrome_driver.page_source
 
-
-def test_fillings_section():
-    driver = webdriver.Chrome()
-
-    try:
-        driver.get("https://stellarburgers.nomoreparties.site/")
-        helpers.click_element(driver, Locators.constructor)
-        time.sleep(4)
-        helpers.click_element(driver, Locators.filling_section)
-        time.sleep(4)
-        assert "Начинки" in driver.page_source
-
-    finally:
-        driver.quit()
